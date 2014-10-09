d10dd266823812112223c67d80196789c96de818
jarvis <jarvis@studioakt.com>
2014-10-09 18:14:58 -0400

    Responsive Tweaks
    - updated main divs to bootstrap standards (col)
    - wrapped sections of widgets in divs and added class in styles.css (widget-bg)
    
    Signed-off-by: jarvis <jarvis@studioakt.com>

--- includes/layouts/main/footer.php
+++ includes/layouts/main/footer.php
@@ -1,13 +1,11 @@
 						</div>
-						<?php include('includes/sidebar_right.php'); ?>
-						<span class="clear"></span>
+							<?php include('includes/sidebar_right.php'); ?>
+						<?php include('includes/sidebar_left.php'); ?>
+
 					</div>
+
 				</div>
-				<span class="clearin"></span>
-				<?php include('includes/sidebar_left.php'); ?>
+
 				<span class="clear"></span>
 			</div>
-		</div>
-	</div>
-
-</div>
+	
--- includes/sidebar_right.php
+++ includes/sidebar_right.php
@@ -3,25 +3,30 @@
 	$youtube = $Site->youtube();
 ?>
 
-<div class="skmerchcontent col-lg-12 col-md-12 col-sm-12 col-xs-12">
-	<div class="skmerchcontentin">
-		<h2>SK MERCH //</h2>
-		<div class="row">
-			<?php foreach($merch->merchItems as $product) { ?>
-			<div class="col-xs-6 product">
-				<a href="<?php echo $merch->product($product); ?>" target="_blank"><img class="img-responsive" src="<?= $merch->image($product['PRODUCT_ID'],$product['IMAGE'],'browse_') ?>" alt="<?=$product['PRODUCT']?>" /></a>
+<div class="skmerchcontent col-lg-3 col-lg-push-3 col-md-4 col-sm-4">
+	<div class="widget-bg">
+		<div class="skmerchcontentin widget">
+			<h2>SK MERCH //</h2>
+			<div class="row">
+				<?php foreach($merch->merchItems as $product) { ?>
+				<div class="col-xs-6 product">
+					<a href="<?php echo $merch->product($product); ?>" target="_blank"><img class="img-responsive" src="<?= $merch->image($product['PRODUCT_ID'],$product['IMAGE'],'browse_') ?>" alt="<?=$product['PRODUCT']?>" /></a>
+				</div>
+				<?php } ?>
 			</div>
-			<?php } ?>
+			<span class="clear"></span>
 		</div>
-		<span class="clear"></span>
 	</div>
-
-	<div class="videocontent">
-		<h2>WATCH STEPHEN'S TED TALK //</h2>
-		<div class="video">
-			<img src="<?=$youtube['photo']?>" width="238"  class="img-responsive" height="134" alt="img">
-			<a class="play " href="<?=$youtube['url']?>"><img src="/images/playbt.png" width="50" height="50" alt="Play" title="Play"></a>
-
+	
+	<div class="widget-bg">		
+		<div class="videocontent widget">
+			<h2>WATCH STEPHEN'S TED TALK //</h2>
+			<div class="video">
+				<a class="youtube" href="<?=$youtube['url']?>">
+					<img src="<?=$youtube['photo']?>" width="238"  class="img-responsive" height="134" alt="img">
+					<span class="play"><img src="/images/playbt.png" width="50" height="50" alt="Play" title="Play"></span>
+				</a>
+			</div>
 		</div>
 	</div>
 		
@@ -35,7 +40,7 @@
 
 <script type="text/javascript">
 $(document).ready(function() {
-	$('.play').fancybox({
+	$('.youtube').fancybox({
 		maxWidth	: 800,
 		maxHeight	: 600,
 		fitToView	: false,
