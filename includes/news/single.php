bba5409b6dd38d43f0a03de273b960cfcea43f51
cstudioakt <cory@studioakt.com>
2014-11-04 17:47:08 -0500

    Fixed news single page
    - No db was selected. Switched it to run off the mysqlClass
    
    Signed-off-by: cstudioakt <cory@studioakt.com>

--- includes/news/single.php
+++ includes/news/single.php
@@ -1,17 +1,15 @@
 <?php
-	$sql = "SELECT
-			*
-			FROM news WHERE id = '{$_GET['article']}' AND show_date <= NOW() LIMIT 1";
-	$row = mysql_fetch_assoc(mysql_query($sql));
+	$article = $SQL->fetchAssoc("SELECT * FROM news WHERE id = '{$_GET['article']}' AND show_date <= NOW() LIMIT 1");
+	$article = $article[0];
 ?>
 	<h2 class="page-title">NEWS //</h2>
 	<div class="news-article">
 		<div class="article-title clearfix">
-			<h3><?php echo date('M', $row['date']); ?><span><?php echo date('d', $row['date']); ?></span></h3>
-			<h2><a href="/news/<?php echo $row['id']; ?>"><?php echo $row['title']; ?></a></h2>
+			<h3><?php echo date('M', $article['date']); ?><span><?php echo date('d', $article['date']); ?></span></h3>
+			<h2><a href="/news/<?php echo $article['id']; ?>"><?php echo $article['title']; ?></a></h2>
 		</div>
 		<div class="article-body">
-			<?php echo $row['content']; ?>
+			<?php echo $article['content']; ?>
 		</div>
 	</div>
 	<a href="/" class="btn btn-default">&laquo; more news</a>
